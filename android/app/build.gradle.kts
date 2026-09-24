aca055882b25cdfe1bddaf77c6ad8104452a7021
anbarasanhack2007 <murugananbarasan166@gmail.com>
2026-09-24 12:53:19 +0530

    Fix Android core library desugaring

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,9 +10,11 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
@@ -37,6 +39,10 @@ android {
         }
     }
 }
+
+dependencies {
+        coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+    }
 
 kotlin {
     compilerOptions {
